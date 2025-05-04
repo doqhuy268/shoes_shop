9fa0a3fb403de577c95ac5c196d563f616966fd7
doqhuy268 <94748411+doqhuy268@users.noreply.github.com>
2025-05-04 08:37:07 +0700

    khởi tạo Project SpringBoot Backend

---
+++ shoes_be/src/main/java/com/example/shoes_be/ShoesBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoes_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesBeApplication.class, args);
+    }
+
+}
